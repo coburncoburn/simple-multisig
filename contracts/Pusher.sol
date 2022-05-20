c3e187073bdefc390c310c6b50b01147ecff20a1
Coburn Berry <32463466+coburncoburn@users.noreply.github.com>
2022-05-20 10:06:23 -0400

    simple pusher with simple multisig

---
+++ contracts/Pusher.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.6.11;
+
+interface ERC20 {
+    function transfer(address _to, uint _value) public returns (bool success);
+    function approve(address _spender, uint _value) public returns (bool success);
+}
+
+interface cERC20 {
+  function mint(uint mintAmount) returns (uint)
+  function redeem(uint redeemTokens) returns (uint)
+}
+
+interface Comptroller {
+  function claimComp(address holder) public
+}
+
+contract Pusher {
+  cERC20 constant cUSDC = cERC20(0x39aa39c021dfbae8fac545936693ac917d5e7563);
+  ERC20 constant USDC = ERC20(0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48);
+  ERC20 constant COMP = ERC20(0xc00e94cb662c3520282e6f5717214004a7f26888);
+  comptroller constant comptroller = Comptroller(0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b);
+
+  address immutable _owner;
+
+  constructor(address owner) {
+    USDC.approve(address(cUSDC), -1);
+    owner = _owner;
+  }
+
+  function push() {
+    cUSDC.mint(USDC.balanceOf(address(this)));
+  }
+  
+  // deal with exchange rate on signing client
+  function withdrawTo(uint amount, uint amountCtokens, address recipient) {
+    require(msg.sender == owner);
+    cUSDC.redeem(amountCtokens);
+    USDC.transfer(recipient, amount);
+  }
+
+  function withdrawCompTo(address recipient) {
+    require(msg.sender == owner);
+    comptroller.claimComp();
+    COMP.transfer(recipient, COMP.balanceOf(address(this)));
+  }
+}
